ebc524ed38748abbce53f9a8f86e18cf07b59f84
abhi-ram-M <129652453+abhi-ram-M@users.noreply.github.com>
2025-09-17 11:09:37 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Number of factors/number-of-factors.py
+++ Difficulty: Easy/Number of factors/number-of-factors.py
@@ -6,8 +6,8 @@ class Solution:
         fac = 0
         for i in range(1,int(sqrt(n))+1):
             if n%i == 0:
-                if n//i == i:
-                    fac += 1
-                else :
+                if n // i != i:
                     fac += 2
+                else:
+                    fac += 1
         return fac
