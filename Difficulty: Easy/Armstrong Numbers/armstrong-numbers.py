a66a8de6243680cc3af4fe8ec777d820e9ebdd91
Abhiram Manepalli <129652453+abhi-ram-M@users.noreply.github.com>
2025-12-16 10:42:34 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Armstrong Numbers/armstrong-numbers.py
+++ Difficulty: Easy/Armstrong Numbers/armstrong-numbers.py
@@ -2,16 +2,12 @@
 
 class Solution:
     def armstrongNumber (self, n):
-        if n<100 or n>1000:
-            return
-        sum = 0
-        length = len(str(n))
-        k=n
-
+        # code here 
+        original = n
+        power = len(str(n))
+        result = 0
         while n>0:
-            sum += (n%10)**length
+            result = result + (n % 10)**power
             n = n//10
-        if sum ==k :
-            return True
-        else :
-            return False
+        return result == original
+        
